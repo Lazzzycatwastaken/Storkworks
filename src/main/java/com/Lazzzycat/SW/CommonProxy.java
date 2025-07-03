3cbc61744876adfd9b636b176e8a7b6899ef5196
Lazzzycatwastaken <tomekgorecki269@gmail.com>
2025-07-03 13:51:42 +0200

    dasdjsakdjfalsjlk

--- src/main/java/com/Lazzzycat/SW/CommonProxy.java
+++ src/main/java/com/Lazzzycat/SW/CommonProxy.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.Lazzzycat.SW;
 
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
---
+++ src/main/java/com/Lazzzycat/SW/Systems/WeatherStateHandler.java
@@ -0,0 +1,6 @@
+package com.Lazzzycat.SW.Systems;
+
+public class WeatherStateHandler {
+
+    public void State;
+}
